a0443366de2cebd82a87a25895b3d38bc1fd20a8
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-01 19:59:20 +0300

    Task 2 completed

--- homework_4/main.js
+++ homework_4/main.js
@@ -11,7 +11,34 @@ alert(`Hello ${userName}! How are you?`);
     HW 4.2. Перевірка числа
 */
 
-//type code here
+const num = prompt("Enter the first 3 digit number");
+
+// Check if the value was inserted, if it's a 3 digit value and if it's a number
+const isNanValue = Number.isNaN(+num);
+const isThreeDigitValue = num?.length == 3 && num?.trim()?.length == 3;
+const isOperationCanceled= num == null;
+
+// Declare message text. It will be filled in based on the results of the check.
+let messageText;
+
+if (isOperationCanceled) {
+    messageText = "The operation was canceled";
+} else if (isNanValue) {
+    messageText = "The inserted value is not a number";
+} else if (!isThreeDigitValue) {
+    messageText = "The inserted value is not a 3 digit number";
+} else {
+    const allNumsEqual = num[0] == num[1] == num[2];
+    const containsEqualNums = num[0] == num[1] || num[1] == num[2] || num[2] == num[0];
+    if (containsEqualNums && !allNumsEqual) {
+        messageText = "The value has equal numbers";
+    } else if (allNumsEqual) {
+        messageText = "All numbers of the value are equal";
+    } else {
+        messageText = "Thank you!";
+    }
+}
+alert(messageText);
 
 //  HW 4.2. end
 
