1358f84d2653917e03cd0e0b1e266dff6953f27e
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-05 16:20:57 +0300

    Pretied code

--- homework_5/main.js
+++ homework_5/main.js
@@ -2,9 +2,9 @@
     HW 5.1. Вивід чисел до консолі
 */
 
-let result = '';
-for (let i = 20; i <= 30; i+= 0.5) {
-    result += `${i} `;
+let result = "";
+for (let i = 20; i <= 30; i += 0.5) {
+  result += `${i} `;
 }
 console.log(result);
 
@@ -15,8 +15,8 @@ console.log(result);
 */
 
 const currencyRate = 26;
-for (let i = 10; i <= 100; i+=10) {
-    console.log(`${i * currencyRate} hrivnyas for ${i} dollars.`);
+for (let i = 10; i <= 100; i += 10) {
+  console.log(`${i * currencyRate} hrivnyas for ${i} dollars.`);
 }
 
 // HW 5.2 end
@@ -27,8 +27,8 @@ for (let i = 10; i <= 100; i+=10) {
 
 const numberInput = +prompt("Enter the number");
 let i = 1;
-while (i*i <= numberInput) {
-    console.log(`${i++}`);
+while (i * i <= numberInput) {
+  console.log(`${i++}`);
 }
 
 // HW 5.3 end
@@ -40,11 +40,11 @@ while (i*i <= numberInput) {
 const inputNumber = +prompt("Enter the number");
 let messageText = "Number is prime";
 for (let x = 2; x <= 7; x++) {
-    if (x == 4 || x == 6) continue; //to make the cycle processing faster
-    if (inputNumber%x == 0 && x != inputNumber) {
-        messageText = "Number is not prime";
-        break; //to make the cycle processing faster
-    }
+  if (x == 4 || x == 6) continue; //to make the cycle processing faster
+  if (inputNumber % x == 0 && x != inputNumber) {
+    messageText = "Number is not prime";
+    break; //to make the cycle processing faster
+  }
 }
 console.log(messageText);
 
