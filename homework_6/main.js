5bb1b905f9c1db269ee98016b4c4795399bb88f3
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-09 01:21:36 +0300

    Completed tasks, README not created

---
+++ homework_6/main.js
@@ -0,0 +1,58 @@
+/*
+  HW 6.1. Написати функцію видалення масиву символів
+*/
+
+function modifyString(inputString, elements) {
+  if (typeof inputString != "string") {
+    return "String is required as a first argument";
+  }
+  if (!Array.isArray(elements)) {
+    return "Array of elements is required as a second argument";
+  }
+  for (item of elements) {
+    inputString = inputString.replaceAll(item, "");
+  }
+  return inputString;
+}
+
+/*
+  HW 6.2. Визначення середнього арифметичного.
+*/
+
+function arithmeticMean(inputValues) {
+  if (!Array.isArray(inputValues)) {
+    return "Array of values is required";
+  }
+  if (inputValues.length == 0 && Array.isArray(inputValues)) {
+    return "A non-empty array is required";
+  }
+  let indexCounter = 0; //to count the actual numbers in the array
+  let sum = 0;
+  inputValues.forEach((item) => {
+    if (typeof item == "number" && !isNaN(item)) {
+      //isNaN added cause there can be NaN in the array that is a number data type
+      indexCounter++;
+      sum += item;
+    }
+  });
+  if (sum != 0 || indexCounter != 0) {
+    return sum / indexCounter;
+  }
+  return "I cannot count arithmetic mean, no number is provided in the array";
+}
+
+/*
+  HW 6.3. Функція видалення елементу
+*/
+
+function removeElement(inputArray, inputItem) {
+  if (!Array.isArray(inputArray)) {
+    return "Array is required";
+  }
+  inputArray.forEach((item, index) => {
+    if (item == inputItem) {
+      inputArray.splice(index, 1);
+    }
+  });
+  return inputArray;
+}
