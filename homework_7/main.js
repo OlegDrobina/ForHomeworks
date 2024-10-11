51f6442a0518745503d00e0126d8fcfb81f49b1f
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-11 11:24:35 +0300

    Tasks 1 and 2 completed

--- homework_7/main.js
+++ homework_7/main.js
@@ -1,3 +1,21 @@
 /*
   HW 7.1. Замикання
 */
+
+function increaseValue() {
+  let storedValue = 0;
+  return function performIncrement(passedValue) {
+    storedValue += passedValue;
+    return storedValue;
+  };
+}
+
+/*
+  HW 7.2. Добуток через карування
+*/
+
+function multiplyNumbers(firstNum) {
+  return (secondNum) => {
+    return firstNum * secondNum;
+  };
+}
