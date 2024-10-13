57d6ac54809ec3656938870980e374d01c7694ab
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-13 23:05:36 +0300

    Completed tasks

--- homework_7/main.js
+++ homework_7/main.js
@@ -19,3 +19,19 @@ function multiplyNumbers(firstNum) {
     return firstNum * secondNum;
   };
 }
+
+/*
+  HW 7.3. Функція з циклом
+*/
+
+function questionUser() {
+  for (let i = 0; i < 10; i++) {
+    const userAnswer = prompt("Enter the number greater than 100");
+    const isNanInserted = isNaN(userAnswer);
+    if (userAnswer < 100) continue;
+    if (userAnswer > 100 || i == 9 || isNanInserted) {
+      console.log(userAnswer);
+      break;
+    }
+  }
+}
