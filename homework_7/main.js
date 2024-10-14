0f3e8647ab4e1031654390abdedb5d9f297d83a6
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-14 19:36:01 +0300

    modification of task 3

--- homework_7/main.js
+++ homework_7/main.js
@@ -28,6 +28,7 @@ function multiplyNumbers(firstNum) {
   HW 7.3. Функція з циклом
 */
 
+//Original function (Option 1)
 function questionUser() {
   for (let i = 0; i < 10; i++) {
     const userAnswer = prompt("Enter the number greater than 100");
@@ -38,4 +39,31 @@ function questionUser() {
       break;
     }
   }
+}
+
+//Option 2
+function questionUser() {
+  const receivedInputProcessor = processReceivedInput();
+  let shouldStopProcesor = false;
+  for (let i = 0; i < 10; i++) {
+    const userAnswer = prompt("Enter the number greater than 100");
+    if (isNaN(userAnswer) || userAnswer > 100 || i == 9) {
+      shouldStopProcesor = true;
+      return receivedInputProcessor(userAnswer, shouldStopProcesor);
+    }
+    receivedInputProcessor(userAnswer, shouldStopProcesor);
+  }
+}
+
+function processReceivedInput() {
+  let storedValue = 0;
+
+  return function (receivedInput, shouldStopProcesor) {
+    if (!isNaN(receivedInput) && receivedInput > storedValue) {
+      storedValue = +receivedInput;
+    }
+    if (shouldStopProcesor) {
+      return +storedValue;
+    }
+  };
 }
