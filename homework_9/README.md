e0bf12a86ec870f37bff2ce8888f43e5f6f1ba35
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-10-20 17:04:17 +0300

    The task is completed

--- homework_9/README.md
+++ homework_9/README.md
@@ -1,3 +1,5 @@
 HW 9.1. Сумма зарплат
 Дізнатись суму всіх зарплат користувачів:
 Об'єкт може містити невідому кількість департаментів та співробітників
+
+This was done using recursion and tested using the sample random object created based on the description of the task. The result returned by the calculator using the sample object is 22000.
---
+++ homework_9/main.js
@@ -0,0 +1,58 @@
+/*
+Sample object for tests
+
+const obj = {
+  sales: [
+    { name: "Yurii", salary: 1000 },
+    { name: "Peter", salary: 1500 },
+    { name: "Vasya", salary: 2000 },
+  ],
+  development: {
+    web: [{ name: "Andrew", salary: 2000 }],
+    internals: [{ name: "Anna", salary: 5000 }],
+    additional: {
+      add1: [{ name: "Kate", salary: 1000 }],
+      add2: [{ name: "Kolya", salary: 2000 }],
+      add3: {
+        add4: {
+          add5: [{ name: "Valera", salary: 3000 }],
+          add6: {
+            add7: [{ name: "Igor", salary: 2500 }],
+          },
+          add8: [{ name: "Sergii", salary: 2000 }],
+        },
+      },
+    },
+  },
+};
+
+*/
+
+/*
+  The following function should be called as:
+  const salaryCalculator = salaryCalculatorWorker();
+  salaryCalculator(obj);
+*/
+function salaryCalculatorWorker() {
+  let totalSalary = 0;
+
+  return function internalProcessCalculation(departmentDivision) {
+    for (department in departmentDivision) {
+      const deparmentStructure = departmentDivision[department];
+      if (Array.isArray(deparmentStructure)) {
+        totalSalary += internalCalculateDepartmentSalary(deparmentStructure);
+      } else {
+        internalProcessCalculation(deparmentStructure);
+      }
+    }
+    return totalSalary;
+  };
+
+  function internalCalculateDepartmentSalary(deparmentStructure) {
+    let deparmentSalary = 0;
+    deparmentStructure.forEach((item) => {
+      deparmentSalary += item.salary;
+    });
+    return deparmentSalary;
+  }
+}
