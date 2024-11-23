1b541e5c20f788f72e9e4a82c93f710c94a1a7ab
Oleg Drobina <oleg.drobina.job@ukr.net>
2024-11-23 22:40:07 +0200

    getBattery method try catch

--- homework_19/main.js
+++ homework_19/main.js
@@ -190,12 +190,17 @@ function getWeekDayFromDay(day) {
 */
 
 function getBatteryLevel() {
-  const newBatteryPromise = navigator.getBattery();
-  newBatteryPromise.then((result) => {
-    const bateryInfoEl = document.getElementById("batery-info");
-    const bateryLevel = result.level * 100;
-    bateryInfoEl.textContent = `Batery level: ${bateryLevel}%`;
-  });
+  //TODO: add processing of this method in different browsers. Currently overcomed with try/catch
+  try {
+    const newBatteryPromise = navigator.getBattery();
+    newBatteryPromise.then((result) => {
+      const bateryInfoEl = document.getElementById("batery-info");
+      const bateryLevel = result.level * 100;
+      bateryInfoEl.textContent = `Batery level: ${bateryLevel}%`;
+    });
+  } catch (exception) {
+    console.log(exception);
+  }
 }
 
 /*
